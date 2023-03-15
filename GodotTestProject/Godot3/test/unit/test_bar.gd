6785ed9aa142cd008e693852cab4a078e1e246a4
Butch Wesley <butch.wesley@gmail.com>
2023-03-15 02:20:19 -0400

    test with godot 3, it works

--- GodotTestProject/Godot3/test/unit/test_bar.gd
+++ GodotTestProject/Godot3/test/unit/test_bar.gd
@@ -1,7 +1,15 @@
-extends 'res://addons/gut/test.gd'
+extends GutTest
+
+class SomeBaseTestClass:
+	extends GutTest
+
+	func foo():
+		return 'bar'
+
+
 
 class TestBarClassOne:
-	extends 'res://addons/gut/test.gd'
+	extends GutTest
 
 	func test_bar_one():
 		pass_test('this passes')
@@ -14,7 +22,7 @@ class TestBarClassOne:
 
 
 class TestBarClassTwo:
-	extends 'res://addons/gut/test.gd'
+	extends GutTest
 
 	func test_bar_one():
 		pass_test('this passes')
@@ -24,3 +32,19 @@ class TestBarClassTwo:
 
 	func test_pending():
 		pending('this test is pending')
+
+
+class TestUsingBaseClass:
+	extends SomeBaseTestClass
+
+	func test_one_thing():
+		assert_eq('just one more thing', 'no way!')
+
+	func test_something_else():
+		assert_ne('could not think of another', 'witty thing to type')
+
+func test_at_the_end_1():
+	assert_true(true)
+
+func test_at_the_end_2():
+	assert_false(false)
